618dd28fd841f5085f56e0555492a39261acfca0
Thiago Siloé <67393987+ts-80@users.noreply.github.com>
2024-04-08 10:58:43 -0300

    Update conexao.js
    atualizado para secrets

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const { google } = require("googleapis");
 //   const client = await auth.getClient();
+const googleCread = {
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url,
+  "universe_domain": process,env.universe_domain}
 
 async function getAuthSheets() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: "google.json",
+    keyFile: googleCread,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
